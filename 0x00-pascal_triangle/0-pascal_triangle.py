66d76dca2318ae665d3be6ce54e30c5a596e4821
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-04-11 17:29:58 -0500

    flake 8 compliance

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,26 +2,25 @@
 """ Pascal's Triangle """
 from typing import List
 
+
 def pascal_triangle(n: int) -> List:
     """
-    Pascal's Triangle 
-    
+    Pascal's Triangle
+
     Args:
         n (int): triangle's deep
 
     Returns:
         List: Pascal's Triangle List
     """
-    l = []
+    pascal_list = []
     if n <= 0:
-        return l
-    l.append([1])
+        return pascal_list
+    pascal_list.append([1])
     for i in range(1, n):
         tmp = [1]
-        for j in range(len(l[i - 1]) - 1):
-            current = l[i - 1]
-            tmp.append(l[i - 1][j] + l[i - 1][j + 1])
+        for j in range(len(pascal_list[i - 1]) - 1):
+            tmp.append(pascal_list[i - 1][j] + pascal_list[i - 1][j + 1])
         tmp.append(1)
-        l.append(tmp)
-    return l
-        
+        pascal_list.append(tmp)
+    return pascal_list
