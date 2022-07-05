dae2ff66b95d48b0e335c9cb83b7e380698a9bb2
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-07-05 07:44:42 -0500

    init project

---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,11 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void menger(int level);
+char _draw_sponge_(int column, int rows);
+
+#endif /* MENGER_H */
