7150e973e2d8519ab4fae4205156ba61dd1ed7e6
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-07-22 07:25:53 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
 
+""" script that reads stdin line by line and computes metrics """
 
 import sys
 
 
-file_size = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+def printstdin(date, size):
+    """ print information from stdin """
+    print("File size: {:d}".format(size))
+    for i in sorted(date.keys()):
+        if date[i] != 0:
+            print("{}: {:d}".format(i, date[i]))
+
+
+statusCode = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+              "404": 0, "405": 0, "500": 0}
+
+counter = 0
+size = 0
 
 try:
-    for i, line in enumerate(sys.stdin, 1):
-        splited = line.split(" ")
-        print("splited")
-        print(splited)
-        if len(splited) < 2:
-            continue
-        if splited[-2] in status_codes:
-            status_codes[splited[-2]] += 1
-        file_size += eval(splited[-1])
-        if i % 10 == 0:
-            print("File size: {}".format(file_size))
-            for key, value in sorted(status_codes.items()):
-                if value > 0:
-                    print("{}: {}".format(key, value))
-finally:
-    print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+    for line in sys.stdin:
+        if counter != 0 and counter % 10 == 0:
+            printstdin(statusCode, size)
+
+        listStatus = line.split()
+        counter += 1
+
+        try:
+            size += int(listStatus[-1])
+        except:
+            pass
+
+        try:
+            if listStatus[-2] in statusCode:
+                statusCode[listStatus[-2]] += 1
+        except:
+            pass
+    printstdin(statusCode, size)
+
+
+except KeyboardInterrupt:
+    printstdin(statusCode, size)
+    raise
